7c185178caf60760333a2154e66f881491e79c1e
fominmal <fomin.mike.al@gmail.com>
2025-04-14 16:22:40 +0300

    Testing pull request

--- main.py
+++ main.py
@@ -3,21 +3,13 @@ import time
 
 def connect_to_device(port, baudrate=9600, timeout=1):
     try:
-        # Открываем последовательный порт
         ser = serial.Serial(port, baudrate, timeout=timeout)
-        print(f"Подключено к {port} с baudrate {baudrate}")
-
-        # Пример отправки команды устройству
         command = "show version\n"
         ser.write(command.encode())
-
-        # Чтение ответа от устройства
-        time.sleep(1)  # Даем время устройству на ответ
+        time.sleep(1)
         response = ser.read(ser.in_waiting).decode()
         print("Ответ от устройства:")
         print(response)
-
-        # Закрываем последовательный порт
         ser.close()
         print("Соединение закрыто")
 
@@ -25,8 +17,7 @@ def connect_to_device(port, baudrate=9600, timeout=1):
         print(f"Ошибка подключения: {e}")
 
 if __name__ == "__main__":
-    # Укажите порт, к которому подключено ваше устройство
-    port = "COM10"  # Для Windows, например
-    # port = "/dev/ttyUSB0"  # Для Linux, например
+    port = input("Укажите порт, к которому подключено ваше устройство: ")
+    baudrate = input("Укажите baudrate подключения: ")
 
     connect_to_device(port)
